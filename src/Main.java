64490e7619b49d97a8931468227eadf7f55716de
ByCooper <kup.official@yahoo.com>
2023-03-16 14:24:31 +0300

    Сделано ДЗ №5

---
+++ src/Main.java
@@ -0,0 +1,79 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+
+
+    }
+
+    public static void task1() {
+        System.out.println("Задача №1" + "\n");
+        //Код операционной системы (clientOS) для iOS = 0, для Android = 1
+        int clientOS = 1;
+        switch (clientOS) {
+            case 0:
+                System.out.println("Установите версию приложения для iOS по ссылке");
+                break;
+            case 1:
+                System.out.println("Установите версию приложения для Android по ссылке");
+                break;
+
+        }
+    }
+
+    public static void task2() {
+        System.out.println("\n" + "Задача №2" + "\n");
+        int clientOS = 0;
+        int clientDeviceYear = 2018;
+        int yearOfMake = 2015;
+
+        if (clientOS == 0 && clientDeviceYear < yearOfMake) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+
+        } else if (clientOS == 0 && clientDeviceYear >= yearOfMake) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+
+        } else if (clientOS == 1 && clientDeviceYear < yearOfMake) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (clientOS == 1 && clientDeviceYear >= yearOfMake) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+    }
+
+    public static void task3() {
+        System.out.println("\n" + "Задача №3" + "\n");
+        //Является год високосным или нет
+        int year = 2021;
+        int everyFour = 4;
+        int everyHundred = 100;
+        int every4Hundred = 400;
+
+        if ((year % everyFour == 0 && year % everyHundred != 0) || year % every4Hundred == 0) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
+    }
+    public static void task4 () {
+        System.out.println("\n" + "Задача №4" + "\n");
+        int deliveryDistance = 95;
+        boolean oneDelivery = deliveryDistance < 20;
+        boolean twoDelivery = deliveryDistance >= 20 && deliveryDistance < 60;
+        boolean threeDelivery = deliveryDistance >= 60 && deliveryDistance < 100;
+
+        if (oneDelivery) {
+            int timeOfDelivery = 1;
+            System.out.println("Потребуется дней: " + timeOfDelivery);
+        } else if (twoDelivery) {
+            int timeOfDelivery = 2;
+            System.out.println("Потребуется дней: " + timeOfDelivery);
+        } else if (threeDelivery) {
+            int timeOfDelivery = 3;
+            System.out.println("Потребуется дней: " + timeOfDelivery);
+        } else {
+            System.out.println("Доставка не осуществляется");
+        }
+    }
+}
